0f0bc0d62f08c420d81cf1374e65be2023720c3c
utkarsh1404 <utkarsh1404@gmail.com>
2017-11-15 17:57:03 -0500

    change paths

---
+++ GANCode/model.py
@@ -0,0 +1,32 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import lasagne
+
+from lasagne.layers import *
+from lasagne.nonlinearities import *
+
+
+
+
+# disc layer description
+lrelu = LeakyRectify(0.1)
+
+input_dis = InputLayer(shape = (None, 3, 128, 128), input_var = input_img)
+frst_conv_layer = batch_norm(Conv2DLayer(input_dis, 20, 3, stride=1, pad=1, nonlinearity=lrelu))
+second_conv_layer = batch_norm(Conv2DLayer(frst_conv_layer, 15, 3, stride=1, pad=1, nonlinearity=lrelu))
+pooled_second_conv_layer = MaxPool2DLayer(second_conv_layer, pool_size=(2,2), stride=2)
+conv_dis_output = ReshapeLayer(pooled_second_conv_layer, ([0], 15*64*64))
+
+
+text_input_dis = InputLayer(shape = (None, 11, 300), input_var = input_text)
+text_input_dis = ReshapeLayer(text_input_dis, ([0], 11*300))
+
+input_fc_dis = ConcatLayer([conv_dis_output, text_input_dis], axis=1)
+frst_hidden_layer = batch_norm(DenseLayer(input_fc_dis, 10000, nonlinearity=lrelu))
+frst_hidden_layer = DropoutLayer(frst_hidden_layer, p=0.35)
+second_hidden_layer = batch_norm(DenseLayer(frst_hidden_layer, 5000, nonlinearity=lrelu))
+second_hidden_layer = DropoutLayer(second_hidden_layer, p=0.35)
+final_output_dis = DenseLayer(second_hidden_layer, 2, nonlinearity = sigmoid)
+
+
